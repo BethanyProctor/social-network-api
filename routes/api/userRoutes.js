b44ea343de4ec9082dc5340925bcb96daa0762ba
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-08-06 15:10:36 -0400

    created user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { getUsers, getOneUser, createUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userControllers')
+
+router.route('/').get(getUsers).post(createUser);
+router.route('/:id').get(getOneUser).put(updateUser).delete(deleteUser);
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router
